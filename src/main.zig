afc9fe27c2fee473876341f8bdac259d47c26c9e
Vidur Khanal <vidurkhanal@gmail.com>
2024-08-17 23:35:31 -0400

    feat: gpa and leak detector

--- src/main.zig
+++ src/main.zig
@@ -1,5 +1,15 @@
+const std = @import("std");
+const heap = std.heap;
+const log = std.log;
 const http_server = @import("http_server.zig");
 
 pub fn main() !void {
-    try http_server.start();
+    var gpa = heap.GeneralPurposeAllocator(.{ .thread_safe = true }){};
+    const allocator = gpa.allocator();
+    {
+        try http_server.start(allocator);
+    }
+    // show potential memory leaks when ZAP is shut down
+    const has_leaked = gpa.detectLeaks();
+    std.log.debug("Has leaked: {}\n", .{has_leaked});
 }
