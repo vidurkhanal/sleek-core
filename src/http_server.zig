44793ceb8bc81f380b0a1fad7f3aac3f61ee0653
Vidur Khanal <vidurkhanal@gmail.com>
2024-08-17 23:35:56 -0400

    feat: json and standard response

--- src/http_server.zig
+++ src/http_server.zig
@@ -1,42 +1,73 @@
 const std = @import("std");
+const mem = @import("std").mem;
 const socket = @import("socket.zig");
 const zap = @import("zap");
+const log = std.log;
 
-var conn: socket.UnixConnection = undefined;
-var gpa = std.heap.GeneralPurposeAllocator(.{ .thread_safe = true }){};
+var uc: socket.UnixConnection = undefined;
+
+const SleekStatus = enum {
+    Ok,
+    Error,
+};
+
+const SleekResponse = struct {
+    status: SleekStatus,
+    response: ?[]const u8,
+
+    fn Error(response: ?[]const u8) SleekResponse {
+        return SleekResponse{ .status = SleekStatus.Error, .response = response };
+    }
+
+    fn Ok(response: ?[]const u8) SleekResponse {
+        return SleekResponse{ .status = SleekStatus.Ok, .response = response };
+    }
+};
 
 fn on_request(r: zap.Request) void {
-    var allocator = gpa.allocator();
-    const response = conn.sendAndReceive(allocator, "hello hello") catch |err| {
-        std.debug.print("Error communicating with Python: {s}\n", .{@errorName(err)});
-        r.sendBody("Internal Server Error") catch |send_err| {
-            std.debug.print("Error sending error response: {s}\n", .{@errorName(send_err)});
-        };
+    r.setContentType(.JSON) catch |err| {
+        log.err("Failed to set content type: {}\n", .{err});
         return;
     };
-    defer allocator.free(response);
 
-    r.sendBody(response) catch |err| {
-        std.debug.print("Error sending response: {s}\n", .{@errorName(err)});
+    var buf: [512]u8 = undefined;
+
+    const response = uc.sendAndReceive(r.body orelse "") catch |err| {
+        const res = SleekResponse.Error("couldn't communicate with sleek socket");
+        log.err("couldn't communicate with sleek socket: {}\n", .{err});
+        if (zap.stringifyBuf(&buf, res, .{ .emit_null_optional_fields = true })) |json| {
+            r.sendBody(json) catch return;
+        } else {
+            r.sendBody("null") catch return;
+        }
+        return;
     };
+
+    const res = SleekResponse.Ok(response);
+    if (zap.stringifyBuf(&buf, res, .{ .emit_null_optional_fields = true })) |json| {
+        r.sendBody(json) catch return;
+    } else {
+        r.sendBody("null") catch return;
+    }
+
+    return;
 }
 
-pub fn start() !void {
-    {
-        conn = try socket.UnixConnection.init("/tmp/sleek.sock");
-        defer conn.deinit();
+pub fn start(allocator: mem.Allocator) !void {
+    uc = try socket.UnixConnection.init(allocator, "/tmp/sleek.sock");
+    defer uc.deinit();
 
-        var listener = zap.HttpListener.init(.{
-            .port = 3000,
-            .on_request = on_request,
-            .log = true,
-        });
-        try listener.listen();
-        std.debug.print("Listening on 0.0.0.0:3000\n", .{});
-        zap.start(.{
-            .threads = 2,
-            .workers = 2,
-        });
-    }
-    std.debug.print("LEAKS: {}\n", .{gpa.detectLeaks()});
+    var listener = zap.Endpoint.Listener.init(allocator, .{
+        .port = 3000,
+        .on_request = on_request,
+        .log = true,
+        .max_clients = 100000,
+    });
+
+    try listener.listen();
+    log.info("Listening on 0.0.0.0:3000\n", .{});
+    zap.start(.{
+        .threads = 8,
+        .workers = 10,
+    });
 }
